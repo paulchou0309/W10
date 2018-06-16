73cc52d13a412c6179007e33fff0f5b55e7add13
zhoujia0309 <1280486226@qq.com>
2018-06-16 09:22:15 +0800

    first commit

---
+++ convert_fcn_dataset.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+import logging
+import os
+
+import cv2
+import numpy as np
+import tensorflow as tf
+from vgg import vgg_16
+from object_detection.utils import dataset_util
+
+
+flags = tf.app.flags
+flags.DEFINE_string('data_dir', '/home/wangpei/tmp/VOC2012', 'Root directory to raw pet dataset.')
+flags.DEFINE_string('output_dir', '/home/wangpei/tmp/output/', 'Path to directory to output TFRecords.')
+
+FLAGS = flags.FLAGS
+
+classes = ['background', 'aeroplane', 'bicycle', 'bird', 'boat',
+           'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable',
+           'dog', 'horse', 'motorbike', 'person', 'potted plant',
+           'sheep', 'sofa', 'train', 'tv/monitor']
+# RGB color for each class
+colormap = [[0, 0, 0], [128, 0, 0], [0, 128, 0], [128, 128, 0], [0, 0, 128],
+            [128, 0, 128], [0, 128, 128], [
+                128, 128, 128], [64, 0, 0], [192, 0, 0],
+            [64, 128, 0], [192, 128, 0], [64, 0, 128], [192, 0, 128],
+            [64, 128, 128], [192, 128, 128], [0, 64, 0], [128, 64, 0],
+            [0, 192, 0], [128, 192, 0], [0, 64, 128]]
+
+
+cm2lbl = np.zeros(256**3)
+for i, cm in enumerate(colormap):
+    cm2lbl[(cm[0] * 256 + cm[1]) * 256 + cm[2]] = i
+
+
+def image2label(im):
+    data = im.astype('int32')
+    # cv2.imread. default channel layout is BGR
+    idx = (data[:, :, 2] * 256 + data[:, :, 1]) * 256 + data[:, :, 0]
+    return np.array(cm2lbl[idx])
+
+
+def dict_to_tf_example(data, label):
+    with open(data, 'rb') as inf:
+        encoded_data = inf.read()
+    img_label = cv2.imread(label)
+    img_mask = image2label(img_label)
+    encoded_label = img_mask.astype(np.uint8).tobytes()
+
+    height, width = img_label.shape[0], img_label.shape[1]
+#    print('the ima height*** %d*******'%height)
+#    print('the ima width*** %d*******'%width)
+    if height < vgg_16.default_image_size or width < vgg_16.default_image_size:
+        # 保证最后随机裁剪的尺寸
+        print('the ima default_image_size*** %d*******'%vgg_16.default_image_size)
+
+        return None
+
+    # Your code here, fill the dict
+    
+    ################
+
+    feature_dict = {
+        'image/height':  dataset_util.int64_feature(height),
+        'image/width': dataset_util.int64_feature(width),
+        'image/filename': dataset_util.bytes_feature(
+          data.encode('utf8')),
+        'image/encoded': dataset_util.bytes_feature(encoded_data),
+        'image/label': dataset_util.bytes_feature(encoded_label),
+        'image/format': dataset_util.bytes_feature('png'.encode('utf8')),
+    }
+    example = tf.train.Example(features=tf.train.Features(feature=feature_dict))
+    return example
+
+
+def create_tf_record(output_filename, file_pars):
+    # Your code here
+  writer = tf.python_io.TFRecordWriter(output_filename)
+  for idx, example in enumerate(classes):
+    if idx % 100 == 0:
+      logging.info('On image %d of %d', idx, len(classes))
+#  image_dir = os.path.join(data_dir, 'images')
+#  annotations_dir = os.path.join(data_dir, 'annotations')
+#  examples_path = os.path.join(annotations_dir, 'trainval.txt')
+#  examples_list = dataset_util.read_examples_list(examples_path)
+
+#  data,lable = zip(*file_pars)
+  
+  try:
+    for datasub,lablesub in list(file_pars):
+      tf_example = dict_to_tf_example(str(datasub),str(lablesub))
+      if tf_example !=None:
+        writer.write(tf_example.SerializeToString())
+  except ValueError:
+    logging.warning('Invalid example: %s, ignoring.', file_pars)
+
+  writer.close()
+
+
+def read_images_names(root, train=True):
+    txt_fname = os.path.join(root, 'ImageSets/Segmentation/', 'train.txt' if train else 'val.txt')
+
+    with open(txt_fname, 'r') as f:
+        images = f.read().split()
+
+    data = []
+    label = []
+    for fname in images:
+        data.append('%s/JPEGImages/%s.jpg' % (root, fname))
+        label.append('%s/SegmentationClass/%s.png' % (root, fname))
+        
+    return zip(data, label)
+
+
+def main(_):
+    logging.info('Prepare dataset file names')
+
+    print('**********begin**************')
+    train_output_path = os.path.join(FLAGS.output_dir, 'fcn_train.record')
+    val_output_path = os.path.join(FLAGS.output_dir, 'fcn_val.record')
+
+    train_files = read_images_names(FLAGS.data_dir, True)
+    val_files = read_images_names(FLAGS.data_dir, False)
+    create_tf_record(train_output_path, train_files)
+    create_tf_record(val_output_path, val_files)
+
+
+if __name__ == '__main__':
+    tf.app.run()
